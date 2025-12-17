3a86bb874b53358c91a79ef868e7beda54ce0ad7
JulioCesar <juliolopezvasquez13@gmail.com>
2025-12-17 16:56:58 -0500

    Agregar archivo index.dart para exportar botones reutilizables y facilitar la importación centralizada.

---
+++ lib/widgets/buttons/index.dart
@@ -0,0 +1,9 @@
+/// Exporta todos los botones reutilizables
+/// 
+/// Facilita la importación centralizada de todos los componentes de botones.
+library buttons;
+
+export 'config_card_button.dart';
+export 'gradient_button.dart';
+export 'date_selector_button.dart';
+export 'floating_chat_button.dart';
